93fcfac7d99f9261a82773ed7402c5fab5db392b
darynwang <daryndewang@gmail.com>
2022-11-05 19:13:43 -0400

    finished other Seller class methods

--- src/Seller.java
+++ src/Seller.java
@@ -25,13 +25,60 @@ public class Seller {
         } else return false;
     }
 
-    public ArrayList<String> getAppointmentRequests() {
+    public ArrayList<String> getBuyerRequests(String time) {
         if (cal == null) {
-            return null;
+            System.out.println("No current calendar for this seller");
         }
-        for (int i = 0; i < cal.getAppointmentCount(); i++) {
+        for (Appointment app : cal.getAppointments()) {
+            if (time.equals(app.getTime())) {
+                return app.getBuyerRequests();
+            }
+        }
+        return null;
 
+    }
+
+    public String confirmAppointmentRequests(String name, String time) {
+        if (cal == null) {
+            System.out.println("No current calendar for this seller");
         }
+        //searches through appointments looking for one that matches the time.
+        for (Appointment app : cal.getAppointments()) {
+            if (time.equals(app.getTime())) {
+                //searches through buyer for one that matches name
+                for (Buyer buyer : app.getBuyerRequests()) {
+                    if (buyer.getName().equals(name)) {
+                        //sets the confirmed buyer to the one with the right name
+                        app.setConfirmedBuyer(buyer);
+                        //clears buyer requests
+                        app.clearBuyerRequests();
+                        //returns the name of the confirmed buyer;
+                        return buyer.getName();
+                    }
+                }
+            }
+        }
+        return "";
+    }
+
+    public ArrayList<Appointment> getUnconfirmedAppointments() {
+        ArrayList<Appointment> apps = new ArrayList<Appointment>();
+        for (Appointment app : cal.getAppointments()) {
+            if (app.getConfirmedBuyer() == null) {
+                apps.add(app);
+            }
+        }
+        return apps;
+    }
+
+    public ArrayList<Appointment> getConfirmedAppointments() {
+        ArrayList<Appointment> apps = new ArrayList<Appointment>();
+        for (Appointment app : cal.getAppointments()) {
+            if (app.getConfirmedBuyer() != null) {
+                apps.add(app);
+            }
+        }
+        return apps;
     }
 
 }
