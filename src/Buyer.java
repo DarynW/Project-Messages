f2c98339bf138bd058131905b8ad7235bbf95a86
Andrew Niu <aniu2240@gmail.com>
2022-11-06 17:55:00 -0500

    Commits java doc stuff

--- src/Buyer.java
+++ src/Buyer.java
@@ -11,6 +11,13 @@ public class Buyer {
     private ArrayList<Appointment> ConfirmedAppointments;
     private ArrayList<Appointment> bRAppointments;
 
+    /**
+     * Instantiates buyer name with param name. ConfirmedAppointments set to empty
+     * RequestedAppointments set to empty.
+     *
+     * @param name  the string to display.  If the text is null,
+     *              the tool tip is turned off for this component.
+     */
     public Buyer(String name) {
         this.name = name;
         ConfirmedAppointments = new ArrayList<Appointment>();
@@ -29,10 +36,21 @@ public class Buyer {
         bRAppointments = new ArrayList<Appointment>();
     }
 
+    /**
+     * gets the buyer's name
+     *
+     * @return returns name variable
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * sets the name using a string param
+     *
+     * @param name The string that you want the
+     *             name to change to.
+     */
     public void setName(String name) {
         this.name = name;
     }
