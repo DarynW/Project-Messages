238dc0ccc44afc25f17532ac1298f8bfb19f749b
Andrew Niu <aniu2240@gmail.com>
2022-11-09 22:51:49 -0500

    Creates main class so I can create Database txt file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        Database db = new Database("database.txt");
+        db.write("1", "dataType", "User");
     }
 }
