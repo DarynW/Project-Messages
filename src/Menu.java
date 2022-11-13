70e9c175e0bbc2a0445beeb7bae56547eb60fc0d
Yash Jha <yashjhawi@gmail.com>
2022-11-13 14:07:08 -0500

    Update Menu.java
    Still need to figure out how to check password and how to get Database name to make a new Seller object

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,27 @@
 //menu that connects to Calendar for the user to interface and login with username and password
 
-public class Menu {
+import javax.swing.*;
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Menu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //ask for username
+        System.out.println("What is your username?");
+        String username = scanner.nextLine();
+
+        //ask for password
+        System.out.println("What is your password?");
+        String password = scanner.nextLine();
+
+        //ask if they are tutor or customer
+        System.out.println("Are you a tutor or customer?");
+        String userObject = scanner.nextLine();
+        if (userObject.toLowerCase(Locale.ROOT).equals("tutor")) {
+            Seller newSeller = new Seller(username);
+        } else if (userObject.toLowerCase(Locale.ROOT).equals("customer")) {
+            Buyer newBuyer = new Buyer(username);
+        }
+    }
 }
