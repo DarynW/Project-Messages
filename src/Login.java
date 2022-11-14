2f98d76719318093e86c7efcf73acc860a1eaf35
Owen-Pryga <58037675+Owen-Pryga@users.noreply.github.com>
2022-11-13 23:04:12 -0500

    Finished createAccount() for Login.java

--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,8 @@
+import javax.xml.crypto.Data;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Handles the login and account creation for the user
@@ -13,10 +15,12 @@ public class Login extends Database {
     private String password;
     private String email;
     private String databaseName;
+    private Database database;
     
     public Login(String databaseName, String userName, String password) {
         super(databaseName);
         this.databaseName = databaseName;
+        database = new Database(this.databaseName);
         this.userName = userName;
         this.password = password;
         this.email = "";
@@ -83,7 +87,53 @@ public class Login extends Database {
     }
     
     public void createAccount() {
-        
+        Scanner scanner = new Scanner(System.in);
+        String newUserName;
+        String newUserPassword;
+        String newUserEmail;
+        String newUserType = "";
+        String documentID = "";
+        ArrayList<String> temp;
+
+        //get username, password, email, and userType
+        System.out.println("Enter your new username");
+        newUserName = scanner.nextLine();
+        System.out.println("Enter a password");
+        newUserPassword = scanner.nextLine();
+        System.out.println("Enter your email");
+        newUserEmail = scanner.nextLine();
+        System.out.println("Select Buyer or Seller\n1. Buyer\n2. Seller");
+        int userType = scanner.nextInt();
+        scanner.nextLine();
+        if (userType == 1) {
+            newUserType = "Buyer";
+        } else if (userType == 2) {
+            newUserType = "Seller";
+        }
+
+        //get documentID number from database
+        try {
+            temp = super.searchAllByField("dataType: User, " +
+                    "username: " + this.userName);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        if (temp.isEmpty()) {
+            documentID = "";
+
+            //write to file in database format
+            try {
+                database.add(documentID, "dataType", "User");
+                database.add(documentID, "userName", newUserName);
+                database.add(documentID, "password", newUserPassword);
+                database.add(documentID, "email", newUserEmail);
+                database.add(documentID, "userType", newUserType);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            System.out.println("Username already exists!");
+        }
     }
     
     public String getUserID() {
